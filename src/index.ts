d4b1060a9d33013aa2e845a017c4ebc40b2d2281
Guilherme Tramontano <tramontano.guiherme@yahoo.com.br>
2022-07-28 05:06:12 -0300

    feat: sketch working lmao

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
 import p5 from 'p5';
 
-const GameInstance = new p5(() => undefined, document.getElementById('root') as HTMLElement)
+const canvas = new p5((game) => {
+    game.width = 250;
+    game.heigt = 250;
+    game.setup = () => game.fill(0);
+    game.update = () => {};
+    return game;
+}, document.getElementById('root') as HTMLElement)
+const framerate = 60;
+const pregame = canvas.createCanvas(250,250)
 
-console.log(GameInstance);
+let size = 0;
 
+const game = {
+    ...canvas,
+    width: 250,
+    height: 250,
+    setup: () => canvas.fill(0),
+    update: (cb: () => any) => {
+        console.log("updating canvas");
+        cb();
+    }
+}
+
+game.setup();
+
+console.log({game});
+
+
+
+const updateCanvas = (g: typeof game) => {
+    g.update(() => {
+        size += 0.1;
+        canvas.stroke(255);
+        canvas.square(10,10, size);
+    })
+    setTimeout(() => updateCanvas(game), 1/framerate);
+}
+
+updateCanvas(game);
